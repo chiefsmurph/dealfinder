cd1e6f1200c173edf864d6f17b66f0587533323b
John Murphy <chiefsmurph@gmail.com>
2016-05-31 13:06:18 -0700

    finished searching parse avg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var async = require('async');
 
-var maxPageSearch = 2;
+var maxPageSearch = 7;
 
 function getPrices(searchQuery) {
   console.log('Searching for completed eBay listings for... "' + searchQuery + '"');
@@ -10,13 +10,16 @@ function getPrices(searchQuery) {
 
   var finishedSearching = function() {
 
-      console.log(JSON.stringify(allSellPrices));
+    var sum = allSellPrices.reduce(function(a, b) { return a + b; });
+    var avg = sum / allSellPrices.length;
+    console.log(JSON.stringify(allSellPrices));
+    console.log('Average Sell Price: $' + avg);
 
   }
 
   var getPricesForPageX = function(num) {
 
-    request('http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Complete=1&LH_Sold=1&_nkw=' + searchQuery + '&_pgn=' + num + '&_skc=100&rt=nc', function (error, response, html) {
+    request('http://www.ebay.com/sch/Cell-Phones-Smartphones/9355/i.html?_from=R40&_sacat=0&LH_Complete=1&LH_Sold=1&_nkw=' + searchQuery + '&_pgn=' + num + '&_skc=100&rt=nc', function (error, response, html) {
       if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         var foundItems = 0;
@@ -24,9 +27,11 @@ function getPrices(searchQuery) {
           var a = $(this);
           var theVal = parseFloat(a.text().trim().substring(1));
           allSellPrices.push(theVal);
-          console.log(theVal + ' on page ' + num);
+          //console.log(theVal + ' on page ' + num);
           foundItems++;
         });
+
+        console.log('found ' + foundItems + ' sold on page ' + num);
 
         if (foundItems && num < maxPageSearch) {
           // RECURSIVE!!!
@@ -44,4 +49,5 @@ function getPrices(searchQuery) {
 
 }
 
-getPrices('sugar');
+var args = process.argv.slice(2);
+getPrices(args.join(' '));
