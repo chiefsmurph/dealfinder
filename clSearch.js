09307eb3c97a95718f2db79d9672894b088f0ea0
John Murphy <chiefsmurph@gmail.com>
2016-06-10 21:14:24 -0700

    now searching for both craigslist and ebay

--- clSearch.js
+++ clSearch.js
@@ -6,10 +6,18 @@ var cheerio = require('cheerio');
 var async = require('async');
 var mongoose = require('mongoose');
 
+var ebaySearch = require('./ebaySearch.js');
+
 var db = mongoose.connect('mongodb://localhost/clListings');
 
 var listingSchema = new mongoose.Schema({
-  clId: Number,
+  clId: {
+    type: Number,
+    set: function (v) {
+      return Math.round(v);
+    }
+  },
+  url: String,
   price: Number,
   title: String,
   make: String,
@@ -32,6 +40,25 @@ var newlisting = new Listing({
 newlisting.save(function(err) {
   if (err) throw err;
 });
+
+
+var saveAll = function( array, cb) {
+
+  console.log('saving');
+  console.log(array);
+  Listing.insertMany(array, cb);
+
+  //
+  // async.forEachSeries(array, function(obj, callback) {
+  //   var newListing = new Listing(obj);
+  //   newlisting.save(function(err, hm) {
+  //     if (err) cb(err);
+  //     console.log(hm);
+  //     callback();
+  //   });
+  // }, cb);
+
+};
 
 // http://sfbay.craigslist.org/search/sss?sort=date
 var getAllClUrlsForSearch = function(params, callback) {
@@ -66,9 +93,9 @@ var getAllClUrlsForSearch = function(params, callback) {
           }
 
           var obj = {
-            name: $(this).children('span').text(),
+            title: $(this).children('span').text(),
             url: 'http://sfbay.craigslist.org' + urlPath,
-            clId: $(this).data('id')
+            clId: Number($(this).data('id'))
           };
 
           allListings.push(obj);
@@ -77,43 +104,50 @@ var getAllClUrlsForSearch = function(params, callback) {
 
         async.forEachSeries(allListings, function(curListing, cb) {
 
-          console.log('looking for ' + JSON.stringify(curListing));
+          // console.log('looking for ' + JSON.stringify(curListing.clId));
           Listing.find({
             clId: curListing.clId
           }, function(err, obj) {
-            if (err) {
+            if (err) throw err;
+            if (obj.length) {
               // listing already in mongo stop early
-              console.log('stopping early because we already found ', curListing);
-              callback();
+              //console.log('skipping something because we already found ', curListing);
+              cb();
             } else {
               // listing has not been found yet
-              responseData.push(obj);
+              // console.log('nah');
+              responseData.push(curListing);
               cb();
             }
-          })
-
-        }), function() {
-
-          console.log('here');
-          // save all new cl listings to mongo
-          Listing.create(responseData, function(err) {
-
-            console.log('there');
-
-            if (err) throw err;
-
-            if (x === params.numPages) {
-              // finished
-              callback(responseData);
-            } else {
-              // move to next page
-              getPage(x + 1);
-            }
-
           });
 
+        }, function() {
 
-        };
+          if (responseData.length) {
+
+            console.log('here', JSON.stringify(responseData));
+            // save all new cl listings to mongo
+            saveAll(responseData, function(err) {
+
+              console.log('there');
+
+              if (err) throw err;
+
+              if (x === params.numPages) {
+                // finished
+                callback(responseData);
+              } else {
+                // move to next page
+                getPage(x + 1);
+              }
+
+            });
+
+          } else {
+            callback(null);
+          }
+
+        });
 
       }
     });
@@ -141,7 +175,7 @@ var getModelFromPage = function(url, cb) {
         make: make,
         price: price
       };
-      console.log(obj);
+      // console.log(obj);
       cb(obj);
     }
   });
@@ -152,44 +186,128 @@ var getModelFromPage = function(url, cb) {
 
 
 var clListings;
+var thoseWithMakeAndModel = [];
+var theBestDeals = [];
+
 async.series([
+  // function(callback) {
+  //
+  //   async.forEachSeries(['http://sfbay.craigslist.org/sby/eld/5629086342.html', 'http://sfbay.craigslist.org/eby/ele/5629172209.html'], function(url, cb) {
+  //
+  //     getModelFromPage(url, function(data) {
+  //         console.log(data);
+  //         cb();
+  //     });
+  //
+  //   }, callback);
+  //
+  // },
   function(callback) {
 
-    async.forEachSeries(['http://sfbay.craigslist.org/sby/eld/5629086342.html', 'http://sfbay.craigslist.org/eby/ele/5629172209.html'], function(url, cb) {
-
-      getModelFromPage(url, function(data) {
-          console.log(data);
-          cb();
-      });
-
-    }, callback);
-
-  },
-  function(callback) {
-
+    // get the urls of all the listings for this search...
     getAllClUrlsForSearch({
+      section: 'ela',
       numPages: 1,
-      min_price: 505,
-      max_price: 510
+      min_price: 303,
+      max_price: 320,
+      sortBy: 'priceasc'
     }, function(clResults) {
       clListings = clResults;
-      console.log('clResults',clResults);
       callback();
     });
 
   },
   function(callback) {
-    var clUrls = clListings.map(function(listing) {
-      return listing.url;
-    });
-    console.log('clUrls', clUrls);
-    async.forEachSeries(clUrls, function(url, cb) {
-      getModelFromPage(url, function(data) {
-          console.log(data);
-          cb();
-      });
-    }, callback);
+    if (clListings) {
+
+      console.log();
+      console.log('found ' + clListings.length + ' new listings...');
+      // take those url's and then get the make. model and price of all those listings
+      var listingCount = 0;
+      async.forEachSeries(clListings, function(listing, cb) {
+        getModelFromPage(listing.url, function(data) {
+            listingCount++;
+            console.log('checking out listing ' + listingCount + ' of ' + clListings.length);
+            data.price = data.price.substring(1); // remove $ sign
+            data.searchedForMakeModel = true;
+
+            var allTheGoodStuff = Object.assign(listing, data);
+
+            if (allTheGoodStuff.make.length > 1 && allTheGoodStuff.model.length > 1) {
+              thoseWithMakeAndModel.push(allTheGoodStuff);
+            }
+
+            console.log(allTheGoodStuff);
+            console.log();
+
+            Listing.update({
+              clId: listing.clId
+            }, data, function(err) {
+              if (err) throw err;
+              cb();
+            })
+
+        });
+      }, callback);
+    } else {
+      console.log('no new listings found since last search');
+      callback();
+    }
+  },
+  function(callback) {
+
+    if (thoseWithMakeAndModel) {
+      console.log('...');
+      console.log('these were the ones with make / model...');
+
+      console.log(thoseWithMakeAndModel);
+
+      async.forEachSeries(thoseWithMakeAndModel, function(listing, cb) {
+        var searchQuery = listing.make + ' ' + listing.model;
+        ebaySearch.getPrices(searchQuery, listing.price, function(avgSellPrice) {
+          console.log(searchQuery);
+          console.log('ebay: ' + avgSellPrice);
+          console.log('selling on cl at ' + listing.price);
+          console.log('');
+          var ebayObj = {
+            ebaySellPrice: Math.round(avgSellPrice * 100) / 100
+          };
+          Listing.update({
+            clId: listing.clId
+          }, ebayObj, function(err) {
+            if (err) throw err;
+            cb();
+          });
+
+          var perc = avgSellPrice / listing.price;
+
+          if ( perc <  0.9 ) {
+            theBestDeals.push(Object.assign(listing, ebayObj));
+          }
+        });
+
+      }, callback);
+
+    } else {
+      callback();
+    }
+
+  },
+  function(callback) {
+
+    // display the best deals
+    console.log('');
+    console.log('THE BEST DEALS...');
+    console.log('craigslist low, ebay high\n');
+
+    console.log(theBestDeals);
+
   }
 ], function() {
+
+
+  // thats all folks display the interesting ones
+
+
   console.log('finished running');
 });
--- ebaySearch.js
+++ ebaySearch.js
@@ -5,7 +5,7 @@ var dataFilters = require('./dataFilters.js');
 
 var maxPageSearch = 3;
 
-function getPrices(searchQuery, sellPrice) {
+function getPrices(searchQuery, sellPrice, cb) {
   console.log('Searching for completed eBay listings for... "' + searchQuery + '" with expected sell price of ' + sellPrice);
   var allSellPrices = [];
   var minSellPrice = sellPrice*.5;
@@ -14,23 +14,24 @@ function getPrices(searchQuery, sellPrice) {
 
     //var outlierFree = dataFilters.removeOutsideSD(removeExtremes(allSellPrices));
     var outlierFree = allSellPrices;
-    console.log('removed ' + (allSellPrices.length - outlierFree.length) + ' from the sell prices');
+    // console.log('removed ' + (allSellPrices.length - outlierFree.length) + ' from the sell prices');
 
     console.log();
     if (outlierFree.length) {
       var sum = outlierFree.reduce(function(a, b) { return a + b; });
       var avg = sum / outlierFree.length;
-      console.log(JSON.stringify(outlierFree));
-      console.log('Average Sell Price: $' + avg);
+      // console.log(JSON.stringify(outlierFree));
+      // console.log('Average Sell Price: $' + avg);
     } else {
       console.log('No sells found.')
     }
+    if (cb) cb(avg || null);
 
   }
 
   var getPricesForPageX = function(num) {
     var requestURL = 'http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Complete=1&LH_Sold=1&_nkw=' + searchQuery + '&_udlo=' + minSellPrice + '&_pgn=' + num + '&_skc=100&rt=nc';
-    console.log(requestURL);
+    // console.log(requestURL);
 
     request(requestURL, function (error, response, html) {
       if (!error && response.statusCode == 200) {
@@ -44,13 +45,13 @@ function getPrices(searchQuery, sellPrice) {
           foundItems++;
         });
 
-        console.log('found ' + foundItems + ' sold on page ' + num);
+        // console.log('found ' + foundItems + ' sold on page ' + num);
 
         if (foundItems && num < maxPageSearch) {
           // RECURSIVE!!!
           getPricesForPageX(num+1);
         } else {
-          console.log('finished on page ' + num);
+          // console.log('finished on page ' + num);
           finishedSearching();
         }
       }
@@ -69,3 +70,7 @@ var lastArg = args.splice(-1, 1)[0];
 console.log(lastArg);
 
 getPrices(args.join(' '), lastArg);
+
+module.exports = {
+  getPrices: getPrices
+};
